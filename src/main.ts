30641ff50692fe79db88c2f1e340e4818f92c128
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-20 00:34:57 +0100

    Initialized Bun environment

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const victoriaClient = () => {
+  return 'victoria-client' // TODO Implement actual functionality
+}
+
+export default victoriaClient
---
+++ test/main.test.ts
@@ -0,0 +1,8 @@
+import {expect, test} from 'bun:test'
+
+const {default: victoriaClient} = await import('#src/main.ts')
+
+test('should run', () => {
+  const result = victoriaClient()
+  expect(result).toBe('victoria-client') // TODO Test actual functionality
+})
