cde7bf826ccbd682f6ac6922a7600d8664b6d6ae
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-20 00:49:53 +0100

    Applied ESLint code fixes

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,6 @@
 import {expect, test} from 'bun:test'
 
 const {default: victoriaClient} = await import('#src/main.ts')
-
 test('should run', () => {
   const result = victoriaClient()
   expect(result).toBe('victoria-client') // TODO Test actual functionality
